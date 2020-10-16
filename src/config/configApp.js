95f81c93726542703fb31f7149789208247075c3
LORY <lorydim44@gmail.com>
2020-10-16 15:23:19 +0200

    Modification config

--- src/config/configApp.js
+++ src/config/configApp.js
@@ -1,20 +1,22 @@
 import firebase from 'firebase';
 
-let config = {
+const config = {
     apiKey: "******************************",
-    authDomain: "****************************",
-    databaseURL: "***************************",
-    projectId: "*****************",
-    storageBucket: "****************",
-    messagingSenderId: "**************",
-    appId: "*******************************",
-    measurementId: "**************"
+    authDomain: "******************************",
+    databaseURL: "******************************",
+    projectId: "******************************",
+    storageBucket: "******************************",
+    messagingSenderId: "******************************",
+    appId: "******************************",
+    measurementId: "******************************"
 };
 
 const Base_URL = "https://api.themoviedb.org/3/";
-const API_KEY = '***************************';
+const API_KEY = '******************************';
 
 const app = firebase.initializeApp(config);
 const db = app.database();
 
-export {Base_URL, API_KEY, db}
+const maListe = db.ref('ListeFilms');
+
+export {Base_URL, API_KEY, firebase, maListe}
